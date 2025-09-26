db1fce7a455d273b7cbe132e9a615e7cb2aee8c8
apriryan29 <apririanto125@gmail.com>
2025-09-26 16:18:22 +0700

    update

--- layoutsurat/cetak_tugas.php
+++ layoutsurat/cetak_tugas.php
@@ -31,4 +31,46 @@
     <div style="text-align: justify; margin-top: 2rem;">
         <p style="font-size: 22px;">Untuk mengadakan Presentasi SPMB Tahun Pelajaran 2025 I 2026 kelas IX pada :</p>
     </div>
+    <table style="font-size: 22px; width: 100%; margin-left: 5rem;">
+        <tr>
+            <td>Hari / Tanggal</td>
+            <td>:</td>
+            <td>Sabtu, 27 September 2025</td>
+        </tr>
+        <tr>
+            <td>Waktu</td>
+            <td>:</td>
+            <td>Pukul 20.00</td>
+        </tr>
+        <tr>
+            <td>Tempat</td>
+            <td>:</td>
+            <td>SMK Muhammadiyah Sampang</td>
+        </tr>
+    </table>
+    <div style="text-align: justify; margin-top: 2rem;">
+        <p style="font-size: 22px;">Demikian surat tugas ini kami buat, agar dilaksanakan dan dipergunakan sebagaimana mestinya.</p>
+    </div>
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td style="font-size: 22px; text-align: left; margin-right: 3rem;">Kepala Sekolah</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td style="font-size: 22px; text-align: left; margin-right: 3rem; padding-top: 6rem;">Budi .....MIASUYAUDsna, S.Sos</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="font-size: 22px; text-align: center; padding-left: 15rem; padding-top:4rem;">Cap dan Tanda Tangan Instansi Terkait</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="font-size: 22px; text-align: center; padding-left: 15rem; padding-top:8rem;">(.............................)</td>
+            <td></td>
+        </tr>
+    </table>
 </div>
