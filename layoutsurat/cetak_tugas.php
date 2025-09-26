e251550000b1e38753afba0c022c40129394289a
apriryan29 <apririanto125@gmail.com>
2025-09-26 11:10:18 +0700

    update program

---
+++ layoutsurat/cetak_tugas.php
@@ -0,0 +1,34 @@
+<?php include 'kopsurat.php' ?>
+
+<div style="font-family: 'Times New Roman'; color: black;  margin-left:3rem;  margin-right:2rem;">
+    <div style="text-align: right; margin-top: 1rem;">
+        <p style=" font-size: 22px;">Sampang, 17 Agustus 1945</p>
+    </div>
+    <div  style="text-align: center; margin-top: 4rem;">
+        <h3 style="color: black;"><u>SURAT TUGAS</u></h3>
+        <p style="font-size: 22px;">Nomor :</p>
+    </div>
+
+
+    <div style="text-align: justify; margin-top: 4rem;">
+        <p style="font-size: 22px;">Yang bertanda tangan dibawah ini Kepala SMK Muhammadiyah Sampang
+            Kabupaten Cilacap memberi tugas sepenuhnya kepada :</p>
+    </div>
+    <table style="font-size: 22px; width: 100%; border: 1px solid black;">
+        <tr>
+            <th style="border: 1px solid black; text-align:center;">No</th>
+            <th style="border: 1px solid black; text-align:center;">Nama</th>
+            <th style="border: 1px solid black; text-align:center;">Jabatan</th>
+            <th style="border: 1px solid black; text-align:center;">Keterangan</th>
+        </tr>
+        <tr>
+            <td style="border: 1px solid black; text-align:center;">1</td>
+            <td style="border: 1px solid black; text-align:center;">ahbdajbdjahdjadadjiad</td>
+            <td style="border: 1px solid black; text-align:center;">Kepala Sekolah</td>
+            <td style="border: 1px solid black; text-align:center;">-</td>
+        </tr>
+    </table>
+    <div style="text-align: justify; margin-top: 2rem;">
+        <p style="font-size: 22px;">Untuk mengadakan Presentasi SPMB Tahun Pelajaran 2025 I 2026 kelas IX pada :</p>
+    </div>
+</div>
