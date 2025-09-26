2f27d1ec92d7486075b94ab0b3f8f168b6f7f219
apriryan29 <apririanto125@gmail.com>
2025-09-26 16:29:14 +0700

    tambah cetak tugas individu

---
+++ layoutsurat/cetak_tugasin.php
@@ -0,0 +1,79 @@
+<?php include 'kopsurat.php' ?>
+
+<div style="font-family: 'Times New Roman'; color: black;  margin-left:3rem;  margin-right:2rem;">
+    <div style="text-align: right; margin-top: 1rem;">
+        <p style=" font-size: 22px;">Sampang, 17 Agustus 1945</p>
+    </div>
+    <div  style="text-align: center; margin-top: 4rem;">
+        <h3 style="color: black;"><u>SURAT TUGAS</u></h3>
+        <p style="font-size: 22px;">Nomor :</p>
+    </div>
+
+
+    <div style="text-align: justify; margin-top: 4rem;">
+        <p style="font-size: 22px;">Yang bertanda tangan dibawah ini Kepala SMK Muhammadiyah Sampang
+            Kabupaten Cilacap memberi tugas sepenuhnya kepada :</p>
+    </div>
+    <table style="font-size: 22px; width: 100%; margin-left: 5rem;">
+        <tr>
+            <td style="padding-right: 4.5rem;">Nama</td>
+            <td>:</td>
+            <td>Ariela Goasajndjadja</td>
+        </tr>
+        <tr>
+            <td>Jabatan</td>
+            <td>:</td>
+            <td>Wakil Kepala Kurikulum</td>
+        </tr>
+        <tr>
+            <td>Unit Kerja</td>
+            <td>:</td>
+            <td>SMK Muhammadiyah Sampang</td>
+        </tr>
+    </table>
+    <div style="text-align: justify; margin-top: 2rem;">
+        <p style="font-size: 22px;">Untuk mengadakan Presentasi SPMB Tahun Pelajaran 2025 I 2026 kelas IX pada :</p>
+    </div>
+    <table style="font-size: 22px; width: 100%; margin-left: 5rem;">
+        <tr>
+            <td>Hari / Tanggal</td>
+            <td>:</td>
+            <td>Sabtu, 27 September 2025</td>
+        </tr>
+        <tr>
+            <td>Waktu</td>
+            <td>:</td>
+            <td>Pukul 20.00</td>
+        </tr>
+        <tr>
+            <td>Tempat</td>
+            <td>:</td>
+            <td>SMK Muhammadiyah Sampang</td>
+        </tr>
+    </table>
+    <div style="text-align: justify; margin-top: 2rem; margin-bottom: 5rem;">
+        <p style="font-size: 22px;">Demikian surat tugas ini kami buat, agar dilaksanakan dan dipergunakan sebagaimana mestinya.</p>
+    </div>
+    <table>
+        <tr>
+            <td style="font-size: 22px; text-align: left; padding-right: 5rem;">Yang Diberi Tugas</td>
+            <td></td>
+            <td style="font-size: 22px; text-align: left; margin-right: 3rem;">Kepala Sekolah</td>
+        </tr>
+        <tr>
+            <td style="font-size: 22px; text-align: left; margin-right: 3rem; padding-top: 6rem;">Ariela Goasajndjadja</td>
+            <td></td>
+            <td style="font-size: 22px; text-align: left; margin-right: 3rem; padding-top: 6rem;">Budi .....MIASUYAUDsna, S.Sos</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="font-size: 22px; text-align: center; padding-top:4rem;">Cap dan Tanda Tangan Instansi Terkait</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td style="font-size: 22px; text-align: center; padding-top:8rem;">(.............................)</td>
+            <td></td>
+        </tr>
+    </table>
+</div>
