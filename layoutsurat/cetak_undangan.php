af7ebd7b84088fe68d091c459dc39c2223f67f0f
apriryan29 <apririanto125@gmail.com>
2025-09-30 10:03:16 +0700

    tambah page cetak_undangan.php

---
+++ layoutsurat/cetak_undangan.php
@@ -0,0 +1,86 @@
+<?php 
+    include'kopsurat.php'
+?>
+
+
+<div style="font-family: 'Times New Roman'; color: black; margin-right: 2rem; margin-left: 1rem;">
+    <table style="font-size: 22px; width: 100%; ">
+        <tr>
+            <td style="width: 7%;">Nomor</td>
+            <td>:</td>
+            <td>208/III.4.AU/A/2025</td>
+            <td style="text-align: end;">Sampang, 7 Maret 2025</td>
+        </tr>
+        <tr>
+            <td>Lamp</td>
+            <td>:</td>
+            <td colspan="2">-</td>
+        </tr>
+        <tr>
+            <td>Perihal</td>
+            <td>:</td>
+            <td colspan="2">Pemberitahuan Zakat Fitrah</td>
+        </tr>
+        <tr>
+            <td colspan="4" style="padding-top: 2rem; padding-left: 5rem;">Kepada Yth.</td>
+        </tr>
+        <tr>
+            <td colspan="4" style="padding-left: 5rem;">Bapak/Ibu Wali Murid</td>
+        </tr>
+        <tr>
+            <td colspan="4" style="padding-left: 5rem;">Di - Tempat</td>
+        </tr>
+    </table>
+
+<!-- ISI SURAT -->
+    <table style="font-size: 22px; width: 90%; margin-left:5rem; text-align: justify;">
+        <tr>
+            <td style="padding-top: 3rem;">Bismillahirohmanirrohim <br> Assalamualaikum wr. wb.</td>
+        </tr>
+        <!-- Pembuka -->
+        <tr>
+            <td style="padding-top: 1rem;">Puji syTrkur kita panjatkan kehadirat Alloh SWT yang telah memberikan taufiq dan
+            hidayah-Nya kepada kita semua, sholar,vat dan salam semoga selalu tercurah kepada junjungan
+            kita Nabi Agung Muhammad SAW dan keluarga para sahabat serta pengikutnya sampai akhir
+            zaman- Amiin.
+            </td>
+        </tr>
+        <!-- Isi Pembuka -->
+        <tr>
+            <td style="padding-top: 1rem;">Dengan ini kami beritahukan kepada Bapak/[bu selaku ora11g tua/rvali rnurid kelas X . XI
+            dan XII bahwa dalam rangka syiar SMK Muhammadiyah Sampang kami menyelenggarakan
+            pengumpulan dan distribusi zakat fitrah. Berdasarkan hal tersebut kami berharap siswa/siswi
+            kelas X. XI, dan XIl dapatmenyalurkanzakat fitrahnyadi sekolah dengan ketentuan sebagai
+            berikut:
+            </td>
+        </tr>
+        <!-- ISI SURAT -->
+        <tr>
+            <td style="padding-top: 1rem;">1. Jika berupa beras, maka seberat 3 Kg
+            2. Jika berupa uang, maka sebesar Rp. 40.000 , (Empat puluh ribu rupiah )
+            3. Untuk pengumpulan Z,akat Fitrah kelas X, XI dan XiI kepada wali kelas
+            masing- masing.
+            4. Zakat Fitrah dikumpulkan rnulai tanggal 10 Maret 2025 sld22Maret2025
+            </td>
+        </tr>
+        <!-- Isi Penutup -->
+        <tr>
+            <td style="padding-top: 1rem;">Demikian pemberitahuan ini kami sampaikan atas perhatian dan kerjasaman,va yang baik
+            kami ucapkan terima kasih" semoga putra/putri Bapak/lbu menjadi anak 1,ang sholih l sholihah
+            </td>
+        </tr>
+        <tr>
+            <td style="padding-top: 1rem;">Jazakumullohu khoiron katsiron <br> Wassalamu'alaikum wr. wb.</td>
+        </tr>
+    </table>
+
+    <!-- Tabel Tanda Tangan -->
+    <table style="font-size: 22px; width: 100%;">
+        <tr>
+            <td style="padding-top: 3rem; padding-left: 45rem;">Kepala Sekolah</td>
+        </tr>
+        <tr>
+            <td style="padding-top: 8rem; padding-left: 45rem;">Budi Martanto, S.S <br>NBM. 1084 462</td>
+        </tr>
+    </table>
+</div>
