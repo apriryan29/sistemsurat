062001678947e5123b4e117f06534d4bde665789
apriryan29 <apririanto125@gmail.com>
2025-09-30 11:05:17 +0700

    commit initial

---
+++ layoutsurat/cetak_keterangan.php
@@ -0,0 +1,91 @@
+<?php
+    include'kopsurat.php'
+?>
+
+<div style="font-family: 'Times New Roman'; color: black;">
+    <div style="text-align: center; margin-top: 4rem;">
+        <table style="width: 100%;">
+            <tr>
+                <td style="font-weight: bold; font-size: 24px; text-align: center;"><u>SURAT KETERANGAN</u></td>
+            </tr>
+            <tr>
+                <td style="font-size: 20px;">NO: </td>
+            </tr>
+        </table>
+
+        <table style="width: 93%; font-size: 22px; text-align:justify; margin:2rem;">
+            <tr>
+                <td colspan="3" style="padding-top: 3rem;">Yang bertanda tangan di bawah ini Kepala SMK Muhammadiirah Sampang Kabupaten Cilacap Propinsi
+                Jawa Tengah, menerangkan bahwa :</td>
+            </tr>
+            <tr>
+                <td style="padding-top: 2rem; width: 40%;">Nama</td>
+                <td style="padding-top: 2rem;">:</td>
+                <td style="padding-top: 2rem;">SIBUYUNG</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tanggal lahir</td>
+                <td>:</td>
+                <td>Cilacap, 3 Februari 1995</td>
+            </tr>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                <td>Cilacap, 3 Februari 1995</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tanggal lahir</td>
+                <td>:</td>
+                <td>Cilacap, 3 Februari 1995</td>
+            </tr>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                <td>Cilacap, 3 Februari 1995</td>
+            </tr>
+            <tr>
+                <td>Tempat, Tanggal lahir</td>
+                <td>:</td>
+                <td>Cilacap, 3 Februari 1995</td>
+            </tr>
+            <tr>
+                <td>NIS</td>
+                <td>:</td>
+                <td>Cilacap, 3 Februari 1995</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="padding-top: 2rem;">Bahwa Orang Tua Tersebut bernama :</td>
+            </tr>
+            <tr>
+                <td>Ijazah Asli dengan nama</td>
+                <td>:</td>
+                <td>Ahmad Suripto</td>
+            </tr>
+            <tr>
+                <td>Kartu Akte dan KK dengan nama</td>
+                <td>:</td>
+                <td>Natim Ahmad Suripto</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="padding-top: 1rem;">Adalah nama orang yang sama atau bukan orang lain. walaupun tertulis dillazah berbeda dengan akte dan
+                KK.</td>
+            </tr>
+            <tr>
+                <td colspan="3" style="padding-top: 1rem;">Demikian surat keterangan ini kami buat dengan sebenar - benarnya. untuk dapat dipergunakan
+                sebagaimana mestinya.</td>
+            </tr>
+        </table>
+    </div>
+    <!-- Tabel Tanda Tangan -->
+    <table style="font-size: 22px; width: 90%;">
+        <tr>
+            <td style="padding-top: 3rem; padding-left: 40rem;">Sampang, 7 Maret 2025</td>
+        </tr>
+        <tr>
+            <td style="padding-left: 40rem;">Kepala Sekolah</td>
+        </tr>
+        <tr>
+            <td style="padding-top: 8rem; padding-left: 40rem;">Budi Martanto, S.S <br>NBM. 1084 462</td>
+        </tr>
+    </table>
+</div>
