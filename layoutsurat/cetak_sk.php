2f587204f7df229c58df6e8e296f096ce1e67fcc
apriryan29 <apririanto125@gmail.com>
2025-10-03 10:56:10 +0700

    lanjutan cetak sk

--- layoutsurat/cetak_sk.php
+++ layoutsurat/cetak_sk.php
@@ -1,99 +1,76 @@
+<?php 
+include 'kopsurat.php';
+include '../include/config.php';
 
+$nomor_surat = $_POST['nomor_surat'];
+$isi = $_POST['isi'];
 
-<?php include 'kopsurat.php' ?>
+if (isset($_POST['tentang'])) {
+    $tentangId = $_POST['tentang'];
 
+    // Ambil data tentang berdasarkan ID
+    $sql_tentang = "SELECT * FROM tb_perihal WHERE id_perihal = $tentangId";
+    $result_tentang = $config->query($sql_tentang);
+    $tentangData = $result_tentang->fetch_assoc();
+}
+?>
 
 <div style="font-family: 'Times New Roman'; color: black;">
     <div style="text-align: center; margin-top: 2rem;">
-        <p style="font-weight: bold; font-size: 24px;"><u>SURAT KEPUTUSAN KEPALA SEKOLAH</u> <br>Nomor :</p>
-        <p style="font-weight: bold; font-size: 24px;">Tentang <br>................</p>
+        <p style="font-weight: bold; font-size: 24px;"><u>SURAT KEPUTUSAN KEPALA SEKOLAH</u> <br>Nomor : <?php echo $nomor_surat; ?></p>
+        <p style="font-weight: bold; font-size: 24px;">Tentang <br><?php echo $tentangData['tentang']; ?></p>
     </div>
 
     <div style="text-align: justify;  padding-left:2rem; padding-right: 3rem; font-size: 22px;">
-        <div style="padding-top: 1rem;">Keputusan Kepala Sekolah</div>
+        <div style="padding-top: 1rem;"><?php echo $tentangData['pembuka']; ?></div>
         <table>
             <tr style="vertical-align: top;">
-                <td style="padding-right: 5rem;">
-                    MEMPERHATIKAN
-                </td>
-                <td style="padding-right: 2rem;"> : </td>
-                <td>Surat Keputusan Kepala SMK Muharnmadiyah Sarnpang Kabupaten Cilacap perihal tugas/bekeria.
-                    2. Usulan Pimpinan Cabang MLrhammadiya hbabdjabudbajdnakdlkasjanbdabhdajh hbahdytadbada ahudheajndkanfkaf anfi</td>
+                <td style="padding-right: 5rem;">MEMPERHATIKAN</td>
+                <td style="padding-right: 2rem;">:</td>
+                <td><?php echo $tentangData['memperhatikan']; ?></td>
             </tr>
             <tr style="vertical-align: top;">
-                <td>
-                    MENIMBANG
-                </td>
-                <td> : </td>
-                <td>Bahw.a untuk kelancaran kegiatan Pendidikan dan Penga.iaran di
-                sekolah Muharnmadiyali, diperlukan Tenaga pengajar atau tenaga
-                kependidikan,
-                2
-                </td>
+                <td>MENIMBANG</td>
+                <td>:</td>
+                <td><?php echo $tentangData['menimbang']; ?></td>
             </tr>
             <tr style="vertical-align: top;">
-                <td>
-                    MENGINGAT
-                </td>
-                <td> : </td>
-                <td>Bahw.a untuk kelancaran kegiatan Pendidikan dan Penga.iaran di
-                sekolah Muharnmadiyali, d
-                </td>
+                <td>MENGINGAT</td>
+                <td>:</td>
+                <td><?php echo $tentangData['mengingat']; ?></td>
             </tr>
             <tr>
                 <td colspan="3" style="text-align: center; padding-top: 2rem;">MEMUTUSKAN</td>
             </tr>
             <tr style="vertical-align: top;">
-                <td style="padding-right: 6rem;">
-                    MENETAPKAN
-                </td>
-                <td> : </td>
+                <td style="padding-right: 6rem;">MENETAPKAN</td>
+                <td>:</td>
                 <td></td>
             </tr>
             <tr style="vertical-align: top;">
-                <td>
-                    Pertama
-                </td>
-                <td style="padding-right: 2rem;"> : </td>
-                <td>Bahw.a untuk kelancaran kegiatan Pendidikan dan Penga.iaran di
-                sekolah Muharnmadiyali, diperlukan Tenaga pengajar atau tenaga
-                kependidikan,
-                2
-                </td>
+                <td>Pertama</td>
+                <td style="padding-right: 2rem;">:</td>
+                <td><?php echo $isi; ?></td>
             </tr>
             <tr style="vertical-align: top;">
-                <td>
-                    Kedua
-                </td>
-                <td> : </td>
-                <td>Bahw.a untuk kelancaran kegiatan Pendidikan dan Penga.iaran di
-                sekolah Muharnmadiyali, d
-                </td>
+                <td>Kedua</td>
+                <td>:</td>
+                <td><?php echo $tentangData['menetapkan_2']; ?></td>
             </tr>
             <tr style="vertical-align: top;">
-                <td>
-                    Ketiga
-                </td>
-                <td> : </td>
-                <td>Bahw.a untuk kelancaran kegiatan Pendidikan dan Penga.iaran di
-                sekolah Muharnmadiyali, d
-                </td>
+                <td>Ketiga</td>
+                <td>:</td>
+                <td><?php echo $tentangData['menetapkan_3']; ?></td>
             </tr>
             <tr style="vertical-align: top;">
-                <td>
-                    Keempat
-                </td>
-                <td> : </td>
-                <td>Bahw.a untuk kelancaran kegiatan Pendidikan dan Penga.iaran di
-                sekolah Muharnmadiyali, d
-                </td>
+                <td>Keempat</td>
+                <td>:</td>
+                <td><?php echo $tentangData['menetapkan_4']; ?></td>
             </tr>
         </table>
-        <div style="padding-top: 1rem;">Surat Keputusan ini disampaikan kepada yang bersangkutan untLrk diketahui dan diperhatikan / dipergunakan
-                sebagaimana mestinya
-        </div>
+        <div style="padding-top: 1rem;"><?php echo $tentangData['penutup']; ?></div>
 
-        <table style="margin-top: 2rem;">
+        <table class="no-break" style="margin-top: 2rem;">
             <tr>
                 <td></td>
                 <td></td>
@@ -106,7 +83,35 @@
                 <td></td>
                 <td style="padding-left: 35rem;">Pada Tanggal</td>
                 <td style="padding-left: 1rem; padding-right: 1rem;">:</td>
-                <td>Sampang</td>
+                <td>
+                    <?php
+                    // Array untuk nama bulan dalam bahasa Indonesia
+                    $bulanIndo = [
+                        1 => 'Januari',
+                        2 => 'Februari',
+                        3 => 'Maret',
+                        4 => 'April',
+                        5 => 'Mei',
+                        6 => 'Juni',
+                        7 => 'Juli',
+                        8 => 'Agustus',
+                        9 => 'September',
+                        10 => 'Oktober',
+                        11 => 'November',
+                        12 => 'Desember'
+                    ];
+
+                    // Ambil tanggal saat ini
+                    $tanggal = date('j'); // Hari
+                    $bulan = $bulanIndo[date('n')]; // Bulan
+                    $tahun = date('Y'); // Tahun
+
+                    // Format tanggal dalam bahasa Indonesia
+                    $tanggalFormat = "$tanggal $bulan $tahun";
+
+                    echo $tanggalFormat;
+                    ?>
+                </td>
             </tr>
             <tr>
                 <td></td>
@@ -127,3 +132,16 @@
         </table>
     </div>
 </div>
+
+<style>
+    @media print {
+        .no-break {
+            page-break-inside: avoid; /* Mencegah pemisahan tabel */
+            width: 100%; /* Memastikan tabel menggunakan lebar penuh */
+        }
+        .no-break tr {
+            page-break-inside: avoid; /* Mencegah pemisahan baris */
+            page-break-after: auto;
+        }
+    }
+</style>
--- out/sk.php
+++ out/sk.php
@@ -1,41 +1,94 @@
+<?php 
+include './include/config.php';
+
+// Ambil data dari tabel tb_kode
+$sql_kode = "SELECT id_kode, kode_surat, pokok_kode FROM tb_kode";
+$result_kode = $config->query($sql_kode);
+
+?>
+
 <!-- Modal untuk Surat Keputusan -->
 <div class="modal fade" id="skModal" tabindex="-1" role="dialog" aria-labelledby="skModalLabel" aria-hidden="true">
-<div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="skModalLabel">Detail Surat Keputusan</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <form method="POST" action="layoutsurat/cetak_sk.php">
-                <div class="form-group">
-                    <label for="nomor-surat">Nomor Surat</label>
-                    <input type="text" class="form-control" name="nomor_surat" placeholder="Contoh: 001/IV.4/A/2025" required>
-                </div>
-                <div class="form-group">
-                    <label for="kode-surat">Nomor Surat</label>
-                    <input type="text" class="form-control" name="kode_surat" placeholder="Contoh: 001/IV.4/A/2025" required>
-                </div>
-                <div class="form-group">
-                    <label for="tanggal">Tanggal</label>
-                    <input class="form-control" name="tanggal" type="date" required>
-                </div>
-                <div class="form-group">
-                    <label for="tentang">Tentang Perihal SK</label>
-                    <input class="form-control" name="tentang" type="text" placeholder="Masukkan Prihal Surat Keputusan" required>
-                </div>
-                <div class="form-group">
-                    <label for="isi">Ketetapan Poin Pertama</label>
-                    <textarea class="form-control" name="isi" placeholder="Masukkan Ketetapan Surat Keputusan" required></textarea>
-                </div>
-                <input type="hidden" name="kategori" value="sk">
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-            </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="skModalLabel">Detail Surat Keputusan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="layoutsurat/cetak_sk.php">
+                    <div class="form-group">
+                        <label for="nomor-surat">Nomor Surat</label>
+                        <input type="text" class="form-control" name="nomor_surat" id="nomor-surat" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="kode-surat">Pilih Kode Surat</label>
+                        <select class="form-control" name="kode_surat" id="kode-surat" required onchange="updateNomorSurat()">
+                            <option value="" disabled selected>Pilih Kode Surat</option>
+                            <?php if ($result_kode->num_rows > 0): ?>
+                                <?php while ($row = $result_kode->fetch_assoc()): ?>
+                                    <option value="<?php echo $row['kode_surat']; ?>">
+                                        <?php echo $row['pokok_kode']; ?>
+                                    </option>
+                                <?php endwhile; ?>
+                            <?php else: ?>
+                                <option value="">Tidak ada kode surat</option>
+                            <?php endif; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="tanggal">Tanggal</label>
+                        <input class="form-control" name="tanggal" type="date" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="tentang">Tentang Perihal SK</label>
+                        <select class="form-control" name="tentang" id="tentang" required>
+                            <option value="" disabled selected>Pilih Tentang</option>
+                            <?php
+                            // Ambil data dari tb_perihal untuk kategori 'sk'
+                            $sql_perihal = "SELECT id_perihal, tentang FROM tb_perihal WHERE kategori = 'sk'";
+                            $result_perihal = $config->query($sql_perihal);
+                            if ($result_perihal->num_rows > 0) {
+                                while ($row = $result_perihal->fetch_assoc()) {
+                                    echo '<option value="' . $row['id_perihal'] . '">' . $row['tentang'] . '</option>';
+                                }
+                            }
+                            ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="isi">Ketetapan Poin Pertama</label>
+                        <textarea type="text" class="form-control" name="isi" placeholder="Masukkan Ketetapan Surat Keputusan" required></textarea>
+                    </div>
+                    <input type="hidden" name="kategori" value="sk">
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+
+<script>
+function updateNomorSurat() {
+    const kodeSuratSelect = document.getElementById('kode-surat');
+    const nomorSuratInput = document.getElementById('nomor-surat');
+    
+    // Ambil kode surat yang dipilih
+    const selectedKode = kodeSuratSelect.value;
+
+    // Dapatkan tahun saat ini
+    const currentYear = new Date().getFullYear();
+
+    // Buat format nomor surat
+    const nomorSurat = "001/IV.4/" + selectedKode + "/" + currentYear;
+
+    // Update input nomor_surat
+    nomorSuratInput.value = nomorSurat;
+}
+</script>
