3c2d5901bc0ac216e48b50906116d21226444e68
apriryan29 <apririanto125@gmail.com>
2025-11-19 13:16:13 +0700

    fungsional surat keterangan

--- layoutsurat/cetak_keterangan.php
+++ layoutsurat/cetak_keterangan.php
@@ -1,5 +1,15 @@
 <?php
-    include'kopsurat.php'
+    include'kopsurat.php';
+
+$nomor_surat = $_POST['nomor_surat'];
+$isi = $_POST['isi'];
+$lahir = $_POST['lahir'];
+$nama = $_POST['nama'];
+$nis = $_POST['nis'];
+$sekolah = $_POST['sekolah'];
+$ortu = $_POST['ortu'];
+$tanggal = $_POST['tanggal'];
+
 ?>
 
 <div style="font-family: 'Times New Roman'; color: black;">
@@ -9,7 +19,7 @@
                 <td style="font-weight: bold; font-size: 24px; text-align: center;"><u>SURAT KETERANGAN</u></td>
             </tr>
             <tr>
-                <td style="font-size: 20px;">NO: </td>
+                <td style="font-size: 20px;">NO: <?php echo $nomor_surat ?></td>
             </tr>
         </table>
 
@@ -21,54 +31,30 @@
             <tr>
                 <td style="padding-top: 2rem; width: 40%;">Nama</td>
                 <td style="padding-top: 2rem;">:</td>
-                <td style="padding-top: 2rem;">SIBUYUNG</td>
+                <td style="padding-top: 2rem;"><?php echo $nama ?></td>
             </tr>
             <tr>
                 <td>Tempat, Tanggal lahir</td>
                 <td>:</td>
-                <td>Cilacap, 3 Februari 1995</td>
+                <td><?php echo $lahir ?></td>
             </tr>
             <tr>
                 <td>NIS</td>
                 <td>:</td>
-                <td>Cilacap, 3 Februari 1995</td>
+                <td><?php echo $nis ?></td>
             </tr>
             <tr>
                 <td>Nama Orang Tua</td>
                 <td>:</td>
-                <td>Ahmad Suripto</td>
+                <td><?php echo $ortu ?></td>
             </tr>
             <tr>
                 <td>Asal Sekolah</td>
                 <td>:</td>
-                <td>SMK Muhammadiyah Sampang</td>
+                <td><?php echo $sekolah ?></td>
             </tr>
             <tr>
-                <td>Nomor Seri Ijazah</td>
-                <td>:</td>
-                <td>DN- SMK 987246113</td>
-            </tr>
-            <tr>
-                <td>Tahun Pelajaran</td>
-                <td>:</td>
-                <td>2012/2013</td>
-            </tr>
-            <tr>
-                <td colspan="3" style="padding-top: 2rem;">Bahwa Orang Tua Tersebut bernama :</td>
-            </tr>
-            <tr>
-                <td>Ijazah Asli dengan nama</td>
-                <td>:</td>
-                <td>Ahmad Suripto</td>
-            </tr>
-            <tr>
-                <td>Kartu Akte dan KK dengan nama</td>
-                <td>:</td>
-                <td>Natim Ahmad Suripto</td>
-            </tr>
-            <tr>
-                <td colspan="3" style="padding-top: 1rem;">Adalah nama orang yang sama atau bukan orang lain. walaupun tertulis dillazah berbeda dengan akte dan
-                KK.</td>
+                <td colspan="3" style="padding-top: 1rem;"><?php echo $isi ?></td>
             </tr>
             <tr>
                 <td colspan="3" style="padding-top: 1rem;">Demikian surat keterangan ini kami buat dengan sebenar - benarnya. untuk dapat dipergunakan
@@ -77,9 +63,45 @@
         </table>
     </div>
     <!-- Tabel Tanda Tangan -->
-    <table style="font-size: 22px; width: 90%;">
+    <table style="font-size: 22px; width: 95%;">
         <tr>
-            <td style="padding-top: 3rem; padding-left: 40rem;">Sampang, 7 Maret 2025</td>
+            <td style="padding-top: 3rem; padding-left: 40rem;">Sampang, 
+                <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        // Ambil tanggal dari input form
+                        $tanggalInput = $_POST['tanggal'];
+
+                        // Mengubah string tanggal menjadi format yang diinginkan
+                        $tanggal = new DateTime($tanggalInput);
+                        
+                        // Array untuk nama bulan dalam bahasa Indonesia
+                        $bulanIndo = [
+                            1 => 'Januari',
+                            2 => 'Februari',
+                            3 => 'Maret',
+                            4 => 'April',
+                            5 => 'Mei',
+                            6 => 'Juni',
+                            7 => 'Juli',
+                            8 => 'Agustus',
+                            9 => 'September',
+                            10 => 'Oktober',
+                            11 => 'November',
+                            12 => 'Desember'
+                        ];
+
+                        // Ambil tanggal dan bulan
+                        $tanggal1 = $tanggal->format('j'); // Hari
+                        $bulan = $bulanIndo[$tanggal->format('n')]; // Bulan
+                        $tahun = $tanggal->format('Y'); // Tahun
+
+                        // Format tanggal dalam bahasa Indonesia
+                        $tanggalFormat = "$tanggal1 $bulan $tahun";
+
+                        echo $tanggalFormat;
+                    }
+                ?>
+                </td>
         </tr>
         <tr>
             <td style="padding-left: 40rem;">Kepala Sekolah</td>
